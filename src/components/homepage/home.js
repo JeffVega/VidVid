8eb681426b4e87ae667c25d9b9424684cf235762
Jeff Vega <jeff.vega321@gmail.com>
2020-06-10 22:17:29 -0700

    getting started button added

--- src/components/homepage/home.js
+++ src/components/homepage/home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Header from '../header/header'
 import BgImage from '../../utils/imgs/bg-image-two.png'
+import 'font-awesome/css/font-awesome.min.css';
+
 const Home = () => {
 
     return (
@@ -14,6 +16,11 @@ const Home = () => {
                 </div>
                 <p>Video calling proivdes quality and latency simply not available with
                     traditional technology</p>
+                    <div className="greet">
+
+                    <a>Get Started</a>
+                    <img src="https://img.icons8.com/flat_round/64/000000/arrow-right.png"/>
+                    </div>
             </div>
             <div className="bg-img">
                 <img src={BgImage} alt="image background"/>
