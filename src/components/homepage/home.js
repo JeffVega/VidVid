35a6edfde0ccef297c34c97b8f4333ce2f3794fd
JeffVega <jeff.vega321@gmail.com>
2020-06-12 00:47:50 -0700

    mobile ready homepage

--- src/components/homepage/home.js
+++ src/components/homepage/home.js
@@ -7,16 +7,21 @@ const Home = () => {
         <div className="homepage">
 
             <Header/>
+            <div className="docker">
+
+           
+
             <div className="info">
-                <div>
+                
                     <h1>Vid2</h1>
                     <h1>The most advanced video chat ever created</h1>
-                </div>
+               
                 <p>Video calling proivdes quality and latency simply not available with
                     traditional technology</p>
             </div>
             <div className="bg-img">
                 <img src={BgImage} alt="image background"/>
+            </div>
             </div>
         </div>
     )
